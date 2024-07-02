beb6d6b1ed9660372da619fe782e8586eeaca816
GavrilS <gavr1lll@abv.bg>
2024-07-03 00:03:47 +0300

    Building the programme engine -> WIP

---
+++ core/engine.py
@@ -0,0 +1,14 @@
+"""
+This file is responsible for running the entire project based on the specified user settings.
+
+Usage:
+python3 engine.py
+"""
+from mail_server_factory.base_server import BaseServerWrapper
+from components.notifying_service import Service
+from components.message_wrapper.basic_message import MessageWrapper
+
+
+
+if __name__=='__main__':
+    pass
