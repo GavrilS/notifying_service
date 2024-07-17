10a724e81ee37ee8155d27fb4c2e3b1f5cf08494
GavrilS <gavr1lll@abv.bg>
2024-07-17 23:30:10 +0300

    Basic engine setup

--- core/engine.py
+++ core/engine.py
@@ -9,6 +9,29 @@ from components.notifying_service import Service
 from components.message_wrapper.basic_message import MessageWrapper
 
 
+def main():
+    # Build the message
+    message_subject = 'Test email notification'
+    message_body = 'This is a test on the email notification service'
+    sender = 'gavr1lll@abv.bg'
+    receivers = [
+        'gsamodivekov@gmail.com'
+    ]
+
+    m = MessageWrapper(message_subject, message_body, None, sender, receivers)
+
+    # Setup the smtp server
+    server = 'smtp.abv.bg'
+    port = 465
+    account = 'gavr1lll@abv.bg'
+    password = ''
+
+    smtp = BaseServerWrapper(server, port, account, password)
+
+    # Setup the notifying service and send the email
+    notify_service = Service(m, smtp)
+    notify_service.send_email()
+
 
 if __name__=='__main__':
-    pass
+    main()
