0f370911c77752ece15a84276b77a72b48e3dffe
GavrilS <gavr1lll@abv.bg>
2024-07-18 07:43:40 +0300

    Updated the engine

--- core/engine.py
+++ core/engine.py
@@ -3,13 +3,15 @@ This file is responsible for running the entire project based on the specified u
 
 Usage:
 python3 engine.py
+python3 core/engine.py --smtp_server '' --smtp_port '' --smtp_acc '' --smtp_acc_pass ''
 """
+from argparse import ArgumentParser
 from mail_server_factory.base_server import BaseServerWrapper
 from components.notifying_service import Service
 from components.message_wrapper.basic_message import MessageWrapper
 
 
-def main():
+def main(**kwargs):
     # Build the message
     message_subject = 'Test email notification'
     message_body = 'This is a test on the email notification service'
@@ -21,10 +23,12 @@ def main():
     m = MessageWrapper(message_subject, message_body, None, sender, receivers)
 
     # Setup the smtp server
-    server = 'smtp.abv.bg'
-    port = 465
-    account = 'gavr1lll@abv.bg'
-    password = ''
+    server = kwargs.get('smtp_server')
+    port = kwargs.get('smtp_port')
+    account = kwargs.get('smtp_acc')
+    password = kwargs.get('smtp_acc_pass')
+
+    print(f"Server: {server}\nPort: {port}\nAcc: {account}\nPassword: {password}")
 
     smtp = BaseServerWrapper(server, port, account, password)
 
@@ -34,4 +38,20 @@ def main():
 
 
 if __name__=='__main__':
-    main()
+    parser = ArgumentParser(description='Provide the necessary data to connect to the SMTP server.')
+    parser.add_argument('--smtp_server')
+    parser.add_argument('--smtp_port')
+    parser.add_argument('--smtp_acc')
+    parser.add_argument('--smtp_acc_pass')
+
+    args = parser.parse_args()
+    smtp_server = args.smtp_server
+    smtp_port = args.smtp_port
+    smtp_acc = args.smtp_acc
+    smtp_acc_pass = args.smtp_acc_pass
+    main(
+        smtp_server=smtp_server, 
+        smtp_port=smtp_port, 
+        smtp_acc=smtp_acc, 
+        smtp_acc_pass=smtp_acc_pass
+    )
